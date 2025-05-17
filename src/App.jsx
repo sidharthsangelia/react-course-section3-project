d9c1c2b2e07ac10d5a71e1b8f9c418fa5938e078
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-05-17 14:20:43 +0530

    split the app component to new components to make it leaner

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
 import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header";
-import TabButton from "./components/TabButton";
-
-import { CORE_CONCEPTS, EXAMPLES } from "./data";
+import { CORE_CONCEPTS } from "./data";
+import Examples from "./components/Examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("");
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
   return (
     <div>
       <header>
@@ -17,47 +11,9 @@ function App() {
       </header>
       <main>
         <CoreConcept data={CORE_CONCEPTS} />
+
+      <Examples />
       </main>
-      <section id="examples">
-        <h2>Examples</h2>
-        <menu>
-          <TabButton
-            isSelected={selectedTopic === "components"}
-            onClick={() => handleSelect("components")}
-          >
-            Components
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "jsx"}
-            onClick={() => handleSelect("jsx")}
-          >
-            JSX
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "props"}
-            onClick={() => handleSelect("props")}
-          >
-            Props
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "state"}
-            onClick={() => handleSelect("state")}
-          >
-            State
-          </TabButton>
-        </menu>
-        {!selectedTopic ? (
-          <p>Please select a topic!</p>
-        ) : (
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
-        )}
-      </section>
     </div>
   );
 }
